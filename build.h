6b34c6bfdeb46edcd4fc33a75af041386f4a97d9
Tom Dealtry <t.dealtry@lancaster.ac.uk>
2017-06-15 15:40:26 +0100

    add some new potential build options

--- build.h
+++ build.h
@@ -5,5 +5,20 @@
 #define __HISTOGRAM_USHORT__
 //#define __HISTOGRAM_UINT__
 
+#define __TOF_TABLE_USHORT__
+//#define __TOF_TABLE_UINT__
+//#define __TOF_TABLE_FLOAT__
+
+#define __TIME_OFFSET_USHORT__
+//#define __TIME_OFFSET_UINT__
+//#define __TIME_OFFSET_FLOAT__
+
+//#define __V3_UINT__
+#define __V3_FLOAT__
+
+//#define __PMT_ID_USHORT__
+#define __PMT_ID_UINT__
+
+//#define __INCREASED_L1_CACHE__
 
 #endif //__BUILD_H__
